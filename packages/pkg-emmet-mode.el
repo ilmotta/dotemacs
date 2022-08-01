37405e6cd179818d319625c09b4ad3916b91eb85
Icaro Motta <icaro.ldm@gmail.com>
2022-08-01 10:13:39 -0300

    Correctly enable emmet-mode in js-mode buffers

--- packages/pkg-emmet-mode.el
+++ packages/pkg-emmet-mode.el
@@ -1,18 +1,17 @@
 ;;; -*- lexical-binding: t; -*-
 
-(defun pkg-emmet-mode/setup ()
-  (when (bound-and-true-p js-jsx-syntax)
-    (emmet-mode +1)))
-
+;;; Commentary:
 ;; https://github.com/smihica/emmet-mode is a minor mode providing support for
 ;; https://emmet.io/'s feature set is a plugin for many popular text editors
 ;; which greatly improves HTML & CSS workflow. Its
 ;; http://docs.emmet.io/abbreviations/ is inspired by CSS selectors.
+
+;;; Code:
 (my/package emmet-mode
   :straight t
   :defer t
-  :hook (js-mode-hook . pkg-emmet-mode/setup)
-  :hook (typescript-tsx-mode-hook . emmet-mode)
+  :hook ((js-mode-hook
+          typescript-tsx-mode-hook) . emmet-mode)
   :init
   (when (bound-and-true-p evil-mode)
     (general-def
