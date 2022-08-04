ad06585f0815cc722ac576e039782df40b00bad0
Icaro Motta <icaro.ldm@gmail.com>
2022-08-04 08:45:42 -0300

    Persist Marginalia's registry

--- packages/pkg-marginalia.el
+++ packages/pkg-marginalia.el
@@ -6,14 +6,24 @@
 
 ;;; Code:
 
+(defun pkg-marginalia/persist-annotator-registry ()
+  (let ((inhibit-message t))
+    (customize-save-variable 'marginalia-annotator-registry
+                             marginalia-annotator-registry)))
+
 (my/package marginalia
   :straight (:host github :repo "minad/marginalia" :branch "main")
   :defer t
+
   :hook (minibuffer-setup-hook . marginalia-mode)
+
   :init
   (general-def
     :keymaps 'minibuffer-local-map
     ;; Similar to the `dired-hide-details-mode' binding.
-    "M-(" #'marginalia-cycle))
+    "M-(" #'marginalia-cycle)
+
+  :config
+  (advice-add #'marginalia-cycle :after #'pkg-marginalia/persist-annotator-registry))
 
 (provide 'pkg-marginalia)
