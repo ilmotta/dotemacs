5b61121a8b35560afc753f7a153c94718ba30516
Icaro Motta <icaro.ldm@gmail.com>
2022-08-04 14:35:18 -0300

    Remove workaround for all-the-icons-dired
    The issue has been fixed already. See
    https://github.com/jtbm37/all-the-icons-dired/issues/34

--- packages/pkg-all-the-icons-dired.el
+++ packages/pkg-all-the-icons-dired.el
@@ -5,11 +5,6 @@
   :when (display-graphic-p)
   :straight t
   :defer t
-  :hook (dired-mode-hook . all-the-icons-dired-mode)
-  :config
-  ;; Re-align icons after creating files/directories.
-  ;; Issue: https://github.com/jtbm37/all-the-icons-dired/issues/34
-  (advice-add 'dired-do-create-files :around #'all-the-icons-dired--refresh-advice)
-  (advice-add 'dired-create-directory :around #'all-the-icons-dired--refresh-advice))
+  :hook (dired-mode-hook . all-the-icons-dired-mode))
 
 (provide 'pkg-all-the-icons-dired)
