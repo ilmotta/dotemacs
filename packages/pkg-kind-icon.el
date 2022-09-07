a7b789586ee562fb4d7d28207269d22166e2f7d1
Icaro Motta <icaro.ldm@gmail.com>
2022-09-07 11:13:06 -0300

    Reset kind-icon cache after scaling up/down text

--- packages/pkg-kind-icon.el
+++ packages/pkg-kind-icon.el
@@ -11,7 +11,6 @@
   :init
   (setq kind-icon-blend-background nil)
   (setq kind-icon-blend-frac 0.12)
-  (setq kind-icon-default-face 'corfu-default)
   (setq kind-icon-default-style '(:padding 0 :stroke 0 :margin 0 :radius 0 :height 1.0 :scale 0.9))
   (setq kind-icon-use-icons t)
 
@@ -20,8 +19,13 @@
 
   :config
   (with-eval-after-load 'corfu
+    (setq kind-icon-default-face 'corfu-default)
     (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
 
-  (advice-add #'load-theme :after #'pkg-kind-icon/reset-cache))
+  ;; Without resetting the cache, icons will sometimes look way bigger/smaller.
+  (advice-add #'load-theme :after #'pkg-kind-icon/reset-cache)
+  (advice-add #'default-text-scale-reset :after #'pkg-kind-icon/reset-cache)
+  (advice-add #'default-text-scale-increase :after #'pkg-kind-icon/reset-cache)
+  (advice-add #'default-text-scale-decrease :after #'pkg-kind-icon/reset-cache))
 
 (provide 'pkg-kind-icon)
