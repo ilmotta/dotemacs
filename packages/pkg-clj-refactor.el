ad73efa09000aa2a75327c9d6a16135d5944aea2
Icaro Motta <icaro.ldm@gmail.com>
2022-12-24 11:30:32 -0300

    Disable magic requires in clj-refactor

--- packages/pkg-clj-refactor.el
+++ packages/pkg-clj-refactor.el
@@ -3,6 +3,10 @@
 (my/package clj-refactor
   :straight t
   :defer t
-  :hook (clojure-mode-hook . clj-refactor-mode))
+  :hook (clojure-mode-hook . clj-refactor-mode)
+  :init
+  ;; The implementation of magic requires blocks Emacs after typing "/", which
+  ;; is very annoying.
+  (setq cljr-magic-requires nil))
 
 (provide 'pkg-clj-refactor)
