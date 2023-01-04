fd1d6c4192dc37d7ecb770e96e60e26be56ca1cd
Icaro Motta <icaro.ldm@gmail.com>
2023-01-04 14:43:36 -0300

    Upgrade browse-to-remote and configure custom host

--- packages/pkg-browse-at-remote.el
+++ packages/pkg-browse-at-remote.el
@@ -6,11 +6,35 @@
 ;;; Code:
 
 (my/package
-  (browse-at-remote :ref "010639fc6bd6c710b56e0f095352da60c92473a3")
+  (browse-at-remote :ref "d81643c975e77d506fe2eb931229739c162adb5d")
   :defer t
   :init
   (general-def
     :keymaps 'pkg-magit/command-map
-    "r" #'browse-at-remote))
+    "r" #'browse-at-remote)
+
+  ;; Use the commit hash. Unfortunately when set to non-nil the package
+  ;; sometimes fails.
+  (setq browse-at-remote-prefer-symbolic nil)
+
+  :config
+  (setq browse-at-remote-remote-type-regexps
+        `((:host ,(rx bol "ilmotta-github" eol)
+           :type "github"
+           :actual-host "github.com")
+          (:host ,(rx bol "github.com" eol)
+           :type "github")
+          (:host ,(rx bol "bitbucket.org" eol)
+           :type "bitbucket")
+          (:host ,(rx bol "gitlab.com" eol)
+           :type "gitlab")
+          (:host ,(rx bol "git.savannah.gnu.org" eol)
+           :type "gnu")
+          (:host ,(rx bol "gist.github.com" eol)
+           :type "gist")
+          (:host ,(rx bol "git.sr.ht" eol)
+           :type "sourcehut")
+          (:host ,(rx bol "gitlab.gnome.org" eol)
+           :type "gitlab"))))
 
 (provide 'pkg-browse-at-remote)
