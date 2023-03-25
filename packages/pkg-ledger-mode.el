b7412941dbe9bd2f0cb64a51096215c474e825f2
Icaro Motta <icaro.ldm@gmail.com>
2023-03-25 13:31:17 -0300

    Add command and keybinding to show default balance report

--- packages/pkg-ledger-mode.el
+++ packages/pkg-ledger-mode.el
@@ -1,5 +1,9 @@
 ;;; -*- lexical-binding: t; -*-
 
+(defun pkg-ledger-mode/report-balance ()
+  (interactive)
+  (ledger-report "bal" nil))
+
 (my/package
   (ledger-mode :ref "8bad528d43007e0310b5e72e6e021b502b30495c")
   :defer t
@@ -8,8 +12,22 @@
   (general-def
     :keymaps 'ledger-mode-map
     "M-j" #'ledger-navigate-next-xact-or-directive
-    "M-k" #'ledger-navigate-prev-xact-or-directive)
+    "M-k" #'ledger-navigate-prev-xact-or-directive
+    "M-J" #'ledger-navigate-next-uncleared
+    "M-K" #'ledger-navigate-previous-uncleared)
 
-  (setq ledger-mode-should-check-version nil))
+  (general-def
+    :keymaps 'ledger-mode-map
+    :states 'normal
+    :prefix my/local-leader
+    "r b" #'pkg-ledger-mode/report-balance)
+
+  (setq ledger-mode-should-check-version nil)
+
+  (setq ledger-reports
+        '(("bal" "%(binary) -f %(ledger-file) bal --real --effective --verify --payee-width 40 --wide  Liabilities Assets")
+          ("reg" "%(binary) -f %(ledger-file) reg")
+          ("payee" "%(binary) -f %(ledger-file) reg @%(payee)")
+          ("account" "%(binary) -f %(ledger-file) reg %(account)"))))
 
 (provide 'pkg-ledger-mode)
