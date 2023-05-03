b4b069039779988c6175645dfd2031dad67c7ef1
Icaro Motta <icaro.ldm@gmail.com>
2023-05-03 09:27:22 -0300

    Update comment

--- core/defaults.el
+++ core/defaults.el
@@ -287,8 +287,8 @@
 ;; I don't use `save-some-buffers' because I use the `magit-status' buffer to be
 ;; asked about saving modified buffers.
 ;;
-;; Given that I have translated C-x to g in `pkg-evil', it's now super
-;; convenient to press `gs' to save buffers (when in normal mode).
+;; Given that I have translated C-x to f in `pkg-evil', it's now super
+;; convenient to press `fs' to save buffers (when in normal mode).
 (global-set-key [remap save-some-buffers] #'save-buffer)
 
 ;;; Long files
