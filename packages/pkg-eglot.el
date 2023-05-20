8ea81fae0d80f3110a9e37304a55c1734309d426
Icaro Motta <icaro.ldm@gmail.com>
2023-05-20 18:29:08 -0300

    Add basic keybindings for eglot

--- packages/pkg-eglot.el
+++ packages/pkg-eglot.el
@@ -3,9 +3,24 @@
 (my/package eglot
   :elpaca nil
   :init
+  (my/general-mode-def
+    :keymaps '(clojure-mode-map
+               go-mode-map
+               go-ts-mode-map)
+    "c a" #'eglot-code-actions
+    "c f d" #'eglot-find-typeDefinition
+    "c f i" #'eglot-find-implementation
+    "c f l" #'eglot-find-declaration
+    "c f r" #'xref-find-references
+    "c r" #'eglot-rename)
+
   ;; Don't block while connecting.
   (setq eglot-sync-connect nil)
-  (setq eglot-autoshutdown t)
+
+  ;; Don't auto-shutdown when the last buffer is killed. I often killed the
+  ;; buffer, but I'll reopen another one in the same project.
+  (setq eglot-autoshutdown nil)
+
   (setq eglot-autoreconnect nil)
   (setq eglot-connect-timeout 30)
   (setq eglot-events-buffer-size 2000000)
