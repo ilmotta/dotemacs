364fa715f7955941d27dee2f89813a1637273771
Icaro Motta <icaro.ldm@gmail.com>
2024-04-11 08:39:52 -0300

    Override default ef-maris-dark bg-main color

--- packages/pkg-ef-themes.el
+++ packages/pkg-ef-themes.el
@@ -6,6 +6,11 @@
   :elpaca (:host github
            :repo "protesilaos/ef-themes"
            :ref "96aaab126510bfd303558e2a9b85a745dab32f41")
-  :defer t)
+  :defer t
+  :init
+  ;; Use `ef-themes-preview-colors-current' to see all available color names.
+  (setq ef-maris-dark-palette-overrides
+        ;; The default bg-main color is too dark.
+        '((bg-main bg-dim))))
 
 (provide 'pkg-ef-themes)
