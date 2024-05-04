6e7e4962977fcb123a94f704191c2666fd31a82b
Icaro Motta <icaro.ldm@gmail.com>
2024-05-04 13:42:55 -0300

    Enable spacious-padding-mode during initialization

--- packages/pkg-spacious-padding.el
+++ packages/pkg-spacious-padding.el
@@ -11,7 +11,7 @@
   :elpaca (:host github
            :repo "protesilaos/spacious-padding"
            :ref "e48f3335f50217e40081631abacc40964150f3ab")
-  :defer t
+  :demand t
   :init
   (setq spacious-padding-subtle-mode-line t)
 
@@ -19,10 +19,13 @@
         '(:fringe-width 25
           :header-line-width 4
           :internal-border-width 6
-          :mode-line-width 6
-          :right-divider-width 45
+          :mode-line-width 0
+          :right-divider-width 1
           :scroll-bar-width 8
           :tab-line-width 6
-          :tab-width 6)))
+          :tab-width 6))
+
+  :config
+  (spacious-padding-mode +1))
 
 (provide 'pkg-spacious-padding)
