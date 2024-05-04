0958b4f868768ac12266e9ea52214644ae8488c7
Icaro Motta <icaro.ldm@gmail.com>
2024-05-04 13:41:07 -0300

    Simplify doom modeline

--- packages/pkg-doom-modeline.el
+++ packages/pkg-doom-modeline.el
@@ -9,25 +9,41 @@ Unfortunately `doom-modeline-def-modeline' is called
 automatically with hardcoded sections and no variable allows me
 to customize the section. There's probably a better way, but this
 works fine."
-  ;; Leave uncommented to overwrite modeline sections
-  ;;
   (doom-modeline-def-modeline 'main
     '(eldoc bar workspace-name
       ;; window-number
       ;; modals
       matches follow buffer-info remote-host buffer-position word-count parrot selection-info)
-    '(compilation objed-state misc-info persp-name battery grip irc mu4e gnus github debug repl lsp minor-modes input-method indent-info buffer-encoding major-mode process
+    '(compilation objed-state misc-info
+      ;; persp-name
+      battery grip
+      ;; irc mu4e gnus
+      github debug repl lsp
+      ;; minor-modes
+      input-method indent-info
+      ;; buffer-encoding major-mode
+      process
       ;; We need to remove vcs because doom-modeline throws error about the vcs section.
       ;; vcs
       check time)))
 
 (defun pkg-doom-modeline/setup-mode ()
   (doom-modeline-mode +1)
+  ;; This setting doesn't take effect if present in the :init section.
+  (setq doom-modeline-bar-width 0)
   (remove-hook 'find-file-hook #'doom-modeline-update-vcs))
 
 (defun pkg-doom-modeline/-font-height-patch (&rest _args)
   "PATCH: Always return the specified `doom-modeline-height'."
   doom-modeline-height)
+
+(defun pkg-doom-modeline/buffer-file-name-filter-return (result)
+  "Transform buffer file name after doom-modeline.
+Use this function to hardcode rules that can't be easily generalized."
+  (let ((rx-status-mobile-context (rx line-start "status-mobile/src/status_im/contexts/")))
+    (cond ((string-match-p rx-status-mobile-context result)
+           (replace-regexp-in-string rx-status-mobile-context "mobile/ctx/" result))
+          (t result))))
 
 (lib-util/pkg doom-modeline
   :elpaca (:ref "e829606301ea71e5f458df45aa53ad3ad882f68e")
@@ -51,11 +67,13 @@ works fine."
   ;;
   (setq doom-modeline-buffer-file-name-style 'relative-from-project)
 
-  (setq doom-modeline-bar-width 0
-        doom-modeline-buffer-encoding nil
+  (setq doom-modeline-buffer-encoding nil
         doom-modeline-buffer-modification-icon t
+        doom-modeline-check-icon nil
+        doom-modeline-check-simple-format t
         doom-modeline-icon (display-graphic-p)
         doom-modeline-irc nil
+        doom-modeline-lsp-icon nil
         doom-modeline-major-mode-icon t
         doom-modeline-minor-modes nil
         doom-modeline-modal nil
@@ -73,6 +91,8 @@ works fine."
 
   :config
   (pkg-doom-modeline/define-sections)
+
+  (advice-add #'doom-modeline--buffer-file-name :filter-return #'pkg-doom-modeline/buffer-file-name-filter-return)
 
   ;; Set the modeline height to the minimum possible value.
   (advice-add #'doom-modeline--font-height :override #'pkg-doom-modeline/-font-height-patch))
