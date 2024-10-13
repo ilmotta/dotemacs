0b80831dfbb7b25e293dc114fa9c750b9fc1f4b9
Icaro Motta <icaro.ldm@gmail.com>
2024-10-12 22:17:18 -0300

    Remove unnecessary config for hl-line-mode

--- packages/pkg-hl-line.el
+++ packages/pkg-hl-line.el
@@ -2,24 +2,6 @@
 
 (require 'lib-util)
 
-(defvar-local pkg-hl-line/mode-on nil
-  "Store the hl-line-mode state for the current buffer.
-
-Disable `hl-line-mode' when selecting text because the selection
-is already visible.")
-
-(defun pkg-hl-line/on-maybe ()
-  (when pkg-hl-line/mode-on
-    (hl-line-mode +1)))
-
-(defun pkg-hl-line/off-maybe ()
-  (when pkg-hl-line/mode-on
-    (hl-line-mode -1)))
-
-(defun pkg-hl-line/update-mode-state ()
-  (when hl-line-mode
-    (setq pkg-hl-line/mode-on t)))
-
 (lib-util/pkg hl-line
   :elpaca nil
   :hook ((conf-mode-hook
@@ -28,14 +10,6 @@ is already visible.")
           prog-mode-hook
           tablist--mode-hook
           text-mode-hook) . hl-line-mode)
-
-  :hook ((evil-visual-state-entry-hook
-          activate-mark-hook ) . pkg-hl-line/off-maybe)
-
-  :hook ((evil-visual-state-exit-hook
-          deactivate-mark-hook ) . pkg-hl-line/on-maybe)
-
-  :hook (hl-line-mode-hook . pkg-hl-line/update-mode-state)
 
   :init
   ;; Not having to render the hl-line overlay in multiple buffers offers a tiny
